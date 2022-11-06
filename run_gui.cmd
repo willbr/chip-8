3e235b8befd55a6198d1425eba2f3fa19425ca96
Will Bettridge-Radford <william.bettridge.radford@gmail.com>
2022-11-06 11:02:24 +0000

    add ttf fonts

--- run_gui.cmd
+++ run_gui.cmd
@@ -6,12 +6,11 @@ set CC=tcc
 reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && (set ARCH=x86) || (set ARCH=x64)
 
 set SDL=.\SDL2-2.24.1
-rem set SDL=..\tinyc.games\_win\SDL2-2.0.5
-rem set TTF=..\_win\SDL2_ttf-2.0.14
+set TTF=.\SDL2_ttf-2.20.1
 
 set PATH=%PATH%;%SDL%\lib\%ARCH%
-rem set PATH=%PATH%;%TTF%\lib\%ARCH%
+set PATH=%PATH%;%TTF%\lib\%ARCH%
 
 rem %CC% -DSDL_MAIN_HANDLED -I%SDL%\include -I%TTF%\include -L%SDL%\lib\%ARCH% -L%TTF%\lib\%ARCH% -lSDL2 -lSDL2_ttf -run src\gui.c
-%CC% -DSDL_MAIN_HANDLED -I%SDL%\include -L%SDL%\lib\%ARCH%  -lSDL2 -run src\gui.c
+%CC% -DSDL_MAIN_HANDLED -I%SDL%\include -I%TTF%\include -L%SDL%\lib\%ARCH%  -L%TTF%\lib\%ARCH% -lSDL2 -lSDL2_ttf -run src\gui.c
 
--- src/gui.c
+++ src/gui.c
@@ -4,11 +4,29 @@
 #define _STDINT_H_
 
 #include <SDL.h>
+#include <SDL_ttf.h>
 
 SDL_Window *window     = NULL;
 SDL_Renderer *renderer = NULL;
 SDL_Texture *screen    = NULL;
 SDL_Rect r;
+
+TTF_Font *font = NULL;
+SDL_Color forecol = { 0xff, 0x00, 0x00, 0xff };
+
+SDL_Texture *regs_1_texture = NULL;
+SDL_Rect regs_1_rect;
+SDL_Texture *regs_2_texture = NULL;
+SDL_Rect regs_2_rect;
+SDL_Texture *regs_3_texture = NULL;
+SDL_Rect regs_3_rect;
+
+#define REG_BUFFER_SIZE 255
+char regs_1[REG_BUFFER_SIZE] = "";
+char regs_2[REG_BUFFER_SIZE] = "";
+char regs_3[REG_BUFFER_SIZE] = "";
+
+void render_regs(void);
 
 int
 main()
@@ -21,6 +39,8 @@ main()
     printf("%s\n", SDL_GetPlatform());
 
     SDL_Init(SDL_INIT_VIDEO);
+
+    TTF_Init();
 
     window = SDL_CreateWindow(
             "title",
@@ -50,14 +70,24 @@ main()
         return 1;
     }
 
+
+    font = TTF_OpenFont("c:\\windows\\fonts\\consola.ttf", 18);
+    if (!font) {
+        fprintf(stderr, "%s\n", TTF_GetError());
+        return 1;
+    }
+
     init();
     load("./roms/IBM Logo.ch8");
 
+    /*
     for (int i = 0; i < 21; i += 1) {
         debug();
         cycle();
         puts("");
     }
+    */
+
 
     while (running) {
         SDL_Event event;
@@ -68,7 +98,18 @@ main()
                     break;
 
                 case SDL_KEYDOWN:
-                    running = SDL_FALSE;
+                    switch (event.key.keysym.sym) {
+                        case SDLK_q:
+                            running = SDL_FALSE;
+                            break;
+                        case SDLK_j:
+                            cycle();
+                            render_regs();
+                            break;
+                        default:
+                            running = SDL_FALSE;
+                            break;
+                    }
                     break;
 
                 default:
@@ -86,7 +127,7 @@ main()
 
         SDL_LockTexture(screen, 0, &pixels, &pitch);
         pixel = pixels;
-        in = &screen_buffer;
+        in = (char*)&screen_buffer;
 
         for (int y = 0; y < 32; y += 1) {
             for (int x = 0; x < 64; x += 1) {
@@ -107,19 +148,23 @@ main()
         SDL_UnlockTexture(screen);
 
         SDL_Rect dest;
-        dest.x = 200;
-        dest.y = 200;
-        dest.w = 64*4;
-        dest.h = 32*4;
+        dest.x = 10;
+        dest.y = 10;
+        dest.w = 64*5;
+        dest.h = 32*5;
         SDL_RenderCopy(renderer, screen, NULL, &dest);
+
+        SDL_RenderCopy(renderer, regs_1_texture, NULL, &regs_1_rect);
+        SDL_RenderCopy(renderer, regs_2_texture, NULL, &regs_2_rect);
+        SDL_RenderCopy(renderer, regs_3_texture, NULL, &regs_3_rect);
 
         SDL_RenderPresent(renderer);
 
         SDL_Delay(16);
     }
 
-
-
+    TTF_CloseFont(font);
+    TTF_Quit();
 
     SDL_DestroyTexture(screen);
     SDL_DestroyRenderer(renderer);
@@ -129,5 +174,45 @@ main()
     puts("goodbye");
 
     return 0;
+}
+
+void
+render_regs(void) {
+    SDL_Surface *text = NULL;
+    snprintf(regs_1, REG_BUFFER_SIZE,
+            "v0=$%02x v1=$%02x v2=$%02x v3=$%02x v4=$%02x v5=$%02x v6=$%02x v7=$%02x"
+            , v[0] , v[1] , v[2] , v[3] , v[4] , v[5] , v[6] , v[7]);
+
+    snprintf(regs_2, REG_BUFFER_SIZE,
+            "v8=$%02x v9=$%02x va=$%02x vb=$%02x vc=$%02x vd=$%02x ve=$%02x vf=$%02x"
+            , v[8] , v[9] , v[0xa] , v[0xb] , v[0xc] , v[0xd] , v[0xe] , v[0xf]);
+
+    snprintf(regs_3, REG_BUFFER_SIZE,
+        "sp=$%03x program_counter=$%03x i=$%03x",
+        stack_pointer, program_counter, i);
+
+    text = TTF_RenderText_Blended(font, regs_1, forecol);
+    regs_1_rect.x = 10;
+    regs_1_rect.y = 200;
+    regs_1_rect.w = text->w;
+    regs_1_rect.h = text->h;
+    regs_1_texture = SDL_CreateTextureFromSurface(renderer, text);
+    SDL_FreeSurface(text);
+
+    text = TTF_RenderText_Blended(font, regs_2, forecol);
+    regs_2_rect.x = 10;
+    regs_2_rect.y = 230;
+    regs_2_rect.w = text->w;
+    regs_2_rect.h = text->h;
+    regs_2_texture = SDL_CreateTextureFromSurface(renderer, text);
+    SDL_FreeSurface(text);
+
+    text = TTF_RenderText_Blended(font, regs_3, forecol);
+    regs_3_rect.x = 10;
+    regs_3_rect.y = 260;
+    regs_3_rect.w = text->w;
+    regs_3_rect.h = text->h;
+    regs_3_texture = SDL_CreateTextureFromSurface(renderer, text);
+    SDL_FreeSurface(text);
 }
 
