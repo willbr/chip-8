be158f663f5fe91979724f52b96d2d0abe96e722
William Bettridge-Radford <william.bettridge.radford@gail.com>
2026-05-04 12:32:40 +0100

    Add hydrate-roms target to make.cmd

--- make.cmd
+++ make.cmd
@@ -7,8 +7,9 @@ if "%1"=="watch-gui" goto watch-gui
 if "%1"=="run-gui" goto run-gui
 if "%1"=="watch-demo" goto watch-demo
 if "%1"=="run-demo" goto run-demo
+if "%1"=="hydrate-roms" goto hydrate-roms
 
-echo Usage: make [watch-cli ^| run-cli ^| watch-gui ^| run-gui ^| watch-demo ^| run-demo]
+echo Usage: make [watch-cli ^| run-cli ^| watch-gui ^| run-gui ^| watch-demo ^| run-demo ^| hydrate-roms]
 exit /b 1
 
 :watch-cli
@@ -33,4 +34,20 @@ exit /b 0
 
 :run-demo
 call run_demo.cmd
+exit /b 0
+
+:hydrate-roms
+echo Hydrating ROMs from dmatlack/chip8 and loktar00/chip8...
+if not exist roms\dmatlack mkdir roms\dmatlack
+if not exist roms\loktar00 mkdir roms\loktar00
+
+git clone --depth 1 https://github.com/dmatlack/chip8.git %TEMP%\chip8-dmatlack >nul 2>&1
+xcopy /Y /S %TEMP%\chip8-dmatlack\roms\games\*.ch8 roms\dmatlack\ >nul 2>&1
+rmdir /S /Q %TEMP%\chip8-dmatlack >nul 2>&1
+
+git clone --depth 1 https://github.com/loktar00/chip8.git %TEMP%\chip8-loktar00 >nul 2>&1
+xcopy /Y /S %TEMP%\chip8-loktar00\roms\*.ch8 roms\loktar00\ >nul 2>&1
+rmdir /S /Q %TEMP%\chip8-loktar00 >nul 2>&1
+
+echo Done.
 exit /b 0
